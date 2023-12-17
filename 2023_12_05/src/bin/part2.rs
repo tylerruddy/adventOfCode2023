e7687a366e4b0f6bf8057c8af234255f04ac623f
tylerruddy <truddy@umich.edu>
2023-12-16 23:57:24 -0500

    Day 5 - Part 2

--- 2023_12_05/src/bin/part2.rs
+++ 2023_12_05/src/bin/part2.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 
 fn main() {
     println!("part2");
@@ -7,65 +8,140 @@ fn main() {
     println!("{result}");
 }
 
+fn parse_starting_ranges(line: String) -> Vec<(u64, u64)> {
+    let v: Vec<u64> = line.split(':')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.trim())
+        .collect::<Vec<_>>()[1]
+        .split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.trim().parse::<u64>().unwrap())
+        .collect();
+    let mut res: Vec<(u64, u64)> = vec![];
+    for i in (1..v.len()).step_by(2) {
+        res.push((v[i-1], v[i-1] + v[i]));
+    }
+    return res;
+}
+
+fn parse_range(line: &String) -> ((u64, u64), (u64, u64)) {
+    let nums: Vec<u64> = line.split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.trim().parse::<u64>().unwrap())
+        .collect();
+    let dest_start = nums[0];
+    let source_start = nums[1];
+    let range = nums[2];
+    return ((source_start, source_start + range), (dest_start, dest_start + range))
+}
+
+// Custom data type that stores ranges in sorted order and extracts ranges, then I make a new one
+// everytime theres a map
+// Extract will return a vec of ranges and will have removed any overlap
+// Adding a range to this type will allow merges
+
+fn extract_overlapping_ranges(dsr: &mut BTreeSet<(u64, u64)>, range: (u64, u64)) -> Vec<(u64, u64)> {
+    let mut overlapping_ranges: Vec<(u64, u64)> = vec![];
+    for r in dsr.iter() {
+        // Overlap
+        if ranges_overlap(*r, range) {
+            overlapping_ranges.push(*r);
+        }
+    }
+    let mut res: Vec<(u64, u64)> = vec![];
+    for r in overlapping_ranges.iter() {
+        dsr.remove(r);
+        // Range leaves some to left and right
+        if r.0 < range.0 && r.1 > range.1 {
+            dsr.insert((r.0, range.0));
+            dsr.insert((range.1, r.1));
+            res.push((range.0, range.1))
+        } // Range leaves some to the right
+        else if r.1 > range.1 {
+            dsr.insert((range.1, r.1));
+            res.push((r.0, range.1))
+        } // Range leaves some to the left
+        else if r.0 < range.0 {
+            dsr.insert((r.0, range.0));
+            res.push((range.0, r.1))
+        } // Range completely engulfed
+        else { 
+            res.push(*r);
+        }
+    }
+    return res;
+}
+
+fn add_range(dsr: &mut BTreeSet<(u64, u64)>, range: (u64, u64)) {
+    let mut overlapping_ranges: Vec<(u64, u64)> = vec![];
+    for r in dsr.iter() {
+        // Overlap
+        if can_merge_ranges(*r, range) {
+            overlapping_ranges.push(*r);
+        }
+    }
+    if overlapping_ranges.is_empty() {
+        dsr.insert(range);
+        return;
+    }
+    for r in overlapping_ranges.iter() {
+        dsr.remove(r);
+    }
+    let smallest = std::cmp::min(range.0, overlapping_ranges.iter().map(|(start, _)| *start).min().unwrap());
+    let largest = std::cmp::max(range.1, overlapping_ranges.iter().map(|(_, end)| *end).max().unwrap());
+    dsr.insert((smallest, largest));
+}
+
+fn ranges_overlap(a: (u64, u64), b: (u64, u64)) -> bool {
+    return (a.0 >= b.0 && a.0 < b.1) || (b.0 >= a.0 && b.0 < a.1);
+}
+
+fn can_merge_ranges(a: (u64, u64), b: (u64, u64)) -> bool {
+    return (a.0 >= b.0 && a.0 <= b.1) || (b.0 >= a.0 && b.0 <= a.1);
+}
+
+fn convert_range(start: (u64, u64), end: (u64, u64), range: (u64, u64)) -> (u64, u64) {
+    let start_diff = range.0 - start.0;
+    let end_diff = range.1 - start.0;
+    return (end.0 + start_diff, end.0 + end_diff);
+}
+
 fn part2(input: &str) -> u64 {
     let lines: Vec<_> = input.lines().map(String::from).collect();
-    let mut v: Vec<bool> = vec![false; 100];
-    let mut temp: Vec<bool> = vec![false; 100];
+    let mut curr_ranges: BTreeSet<(u64, u64)> = Default::default();
+    let mut temp_ranges: BTreeSet<(u64, u64)> = Default::default();
+    let ranges = parse_starting_ranges(lines.iter().next().unwrap().to_string());
+    for range in ranges.iter() {
+        add_range(&mut curr_ranges, *range);
+    }
+    let _ = lines.iter().next();
 
-    for (i, line) in lines.iter().enumerate() {
-        if i == 0 {
-            let s: Vec<_> = line.split(':').collect();
-            let to_parse: Vec<_> = s[1].split(' ')
-                .filter(|x| !x.is_empty() && x.chars().nth(0).unwrap().is_digit(10))
-                .map(|x| x.trim()
-                    .parse::<u64>()
-                    .unwrap())
-                .collect();
-            for i in (1..to_parse.len()).step_by(2) {
-                for seed in to_parse[i-1]..to_parse[i-1]+to_parse[i] {
-                    let pos = seed as usize;
-                    v[pos] = true;
-                    temp[pos] = true;
-                }
-            }
-            continue;
-        }
+    for line in lines.iter() {
         if line.is_empty() {
-            v = temp.clone();
+            for r in temp_ranges.iter() {
+                curr_ranges.insert(*r);
+            }
+            temp_ranges.clear();
             continue;
         }
         if !line.chars().nth(0).unwrap().is_digit(10) {
-            println!("{line}");
             continue;
         }
-        let map = line.split(' ')
-            .map(|x| x.trim()
-                .parse::<u64>()
-                .unwrap())
-            .collect::<Vec<_>>();
-        let dest_start = map[0] as usize;
-        let source_start = map[1] as usize;
-        let range = map[2] as usize;
-        for i in 0..range {
-            if v[source_start+i] {
-                temp[source_start+i] = false;
-                temp[dest_start+i] = true;
-            }
+        let (start_range, dest_range) = parse_range(line);
+
+        let overlapping_ranges = extract_overlapping_ranges(&mut curr_ranges, start_range);
+        for range in overlapping_ranges.iter() {
+            temp_ranges.insert(convert_range(start_range, dest_range, *range));
         }
     }
-
-    v = temp.clone();
-    println!("{:?}", v);
-
-    let mut res = 0;
-    for (i, val) in v.iter().enumerate() {
-        if *val {
-            res = i as u64;
-            break;
-        }
+    for r in temp_ranges.iter() {
+        curr_ranges.insert(*r);
     }
 
-    return res;
+    return curr_ranges.iter()
+        .map(|(start, _)| *start)
+        .min()
+        .unwrap();
 }
 
 #[cfg(test)]
